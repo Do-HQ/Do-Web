ccd81f951eb222a16c559c51f2b4e459f30b5c3b
Ohani Kizito <125478867+kizitech@users.noreply.github.com>
2026-02-03 16:35:42 +0100

    feat(onboarding): restructure onboarding pages and update metadata titles

--- app/(onboarding)/workspace/page.tsx
+++ app/(onboarding)/workspace/page.tsx
@@ -2,7 +2,7 @@ import JoinWorkspace from "@/components/workspace";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Join a Workspace | Squircle",
+  title: "Join a Workspace",
   description:
     "Browse public workspaces and request access to collaborate with teams and contributors.",
 };
